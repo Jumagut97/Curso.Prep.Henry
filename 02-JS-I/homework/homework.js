554b590dbf26945abf277662b888a245887b3fd4
Juan Manuel Gutierrez <jmagtz19mail.com>
2021-10-14 13:56:10 -0500

    Tarea 2 JS terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,9 +73,7 @@ function tienenMismaLongitud(str1, str2) {
   if(str1.length == str2.length){
     return true;
   }
-  else{
     return false;
-  }
 }
 
 function menosQueNoventa(num) {
@@ -83,11 +81,10 @@ function menosQueNoventa(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if(num < 90){
-    return(true);
+    return true;
   }
-  else{
-    return(false);
-  }
+  return false;
+  
 }
 
 function mayorQueCincuenta(num) {
@@ -95,17 +92,16 @@ function mayorQueCincuenta(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if(num > 50){
-    return(true);
+    return true;
   }
-  else{
-    return(false);
-  }
+  return false;
+  
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  return(x % y);
+  return x % y;
 }
 
 function esPar(num) {
@@ -115,9 +111,8 @@ function esPar(num) {
   if(num % 2 == 0){
     return(true);
   }
-  else{
-    return(false);
-  }
+  return false;
+
   
 }
 
@@ -126,11 +121,10 @@ function esImpar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (num % 2 ==1){
-    return(true);
+    return true;
   }
-  else{
-    return(false);
-  }
+  return false;
+  
 }
 
 function elevarAlCuadrado(num) {
@@ -176,13 +170,13 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if(numero === 0){
-    return(false);
+    return false;
   }
   else if (numero > 0){
-    return("Es positivo");
+    return "Es positivo";
   }
   else{
-    return("Es negativo");
+    return"Es negativo";
   }   
 }
 
@@ -190,41 +184,41 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return(str + "!");
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return(nombre + " " + apellido);
+  return nombre + " " + apellido;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return("Hola " + nombre + "!" );
+  return "Hola " + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return(alto * ancho);
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return(lado * 4);
+  return lado * 4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  return((base * altura) / 2);
+  return (base * altura) / 2;
 
 }
 
@@ -233,7 +227,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return(euro * 1.20);
+  return euro * 1.20;
 }
 
 
@@ -244,12 +238,12 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if(letra.length > 1){
-    return("Dato incorrecto");
+    return "Dato incorrecto";
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return("Es vocal");
+    return "Es vocal";
   }
-    return("Dato incorrecto");
+    return "Dato incorrecto";
 }
 
 
